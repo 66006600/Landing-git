2455d8cb8100421c3faab574e78da87b346c6641
Beverly M <124820505+66006600@users.noreply.github.com>
2023-04-05 21:53:28 -0400

    Update Footer.js
    BackgroundColor

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <>
       <footer className="bg-dark text-center text-lg-start">
 
-        <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        <div className="text-center p-3" style="backgroundColor: rgba(0, 0, 0, 0.2);">
           © 2020 Copyright:
           <a className="text-light" href="https://mdbootstrap.com/">MDBootstrap.com</a>
         </div>
