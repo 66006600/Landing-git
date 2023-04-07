d2f379fab59223227435e32b520158d94d4e0999
Beverly M <bevponi@gmail.com>
2023-04-07 16:11:13 +0000

    Ultimos toques

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -6,32 +6,21 @@ function Navbar() {
     <>
       <nav className="navbar navbar-dark bg-dark">
         <div className="container-fluid">
+          <a></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-                      </button>
+          </button>
+        </div>
+
+        <div className="collapse" id="navbarToggleExternalContent">
+          <div className="bg-dark p-4">
+
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Pricing</a>
+          </div>
         </div>
       </nav>
-      <div className="collapse" id="navbarToggleExternalContent">
-        <div className="bg-dark p-4">
-          <h5 className="text-white">Collapsed content</h5>
-          <span className="text-muted">Toggleable via the navbar brand.</span>
-          <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-        </div>
-      </div>
-      
 
     </>
   );
